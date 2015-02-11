152c4f3d8bdcaa7880325f4435be2320e49e80a3
Lucas Crawford <thorrism@gmail.com>
2015-02-11 12:22:23 -0800

    Update BookSelectionScreen.java

--- app/src/main/java/com/addressbook/thorrism/addressbook/BookSelectionScreen.java
+++ app/src/main/java/com/addressbook/thorrism/addressbook/BookSelectionScreen.java
@@ -83,7 +83,7 @@ public class BookSelectionScreen extends Activity {
 
         ParseObject.registerSubclass(AddressBook.class);
         ParseObject.registerSubclass(Contact.class);
-        Parse.initialize(this, "kpVXSqTA4cCxBYcDlcz1gGJKPZvMeofiKlWKzcV3", "T4FqPFp0ufX4qs8rIUDL8EX8RSluB0wGX51ZpL12" );
+        Parse.initialize(this, "###", "###" );
         mBooks = new ArrayList<AddressBook>();
 
         displayBooks();
